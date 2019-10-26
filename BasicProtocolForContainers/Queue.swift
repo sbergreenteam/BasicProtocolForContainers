309f37d24a020f6a88cbf96be46a786d12b88dca
Ikhtiyor Nurmatov <standardmail77@gmail.com>
2019-10-26 16:18:31 -0400

    UI

--- BasicProtocolForContainers/Queue.swift
+++ BasicProtocolForContainers/Queue.swift
@@ -8,27 +8,27 @@
 
 import Foundation
 
+// Классическая универсальная очередь(FIFO)
+
 public struct Queue<T>: CustomStringConvertible, Container {
     private var array = [T]()
-    
 
-    
     public var isEmpty: Bool {
         return array.isEmpty
     }
-    
+
     public var count: Int {
        return array.count
     }
-    
+
     public mutating func enqueue(_ element: T) {
         array.append(element)
     }
-    
+
     public mutating func add(_ item: T) {
         self.enqueue(item)
     }
-    
+
     public mutating func dequeue() -> T? {
         if isEmpty {
             return nil
@@ -36,16 +36,70 @@ public struct Queue<T>: CustomStringConvertible, Container {
             return array.removeFirst()
         }
     }
-    
+
     public var front: T? {
         return array.first
     }
-    
+
     public subscript(position: Int) -> T? {
         return array[position]
     }
-    
+
     public var description: String {
         return array.description
     }
 }
+
+// Оптимизированная универсальная очередь
+
+//public struct Queue<T>: CustomStringConvertible, Container {
+//    fileprivate var array = [T?]()
+//    fileprivate var head = 0
+//
+//    public var isEmpty: Bool {
+//        return count == 0
+//    }
+//
+//    public var count: Int {
+//        return array.count - head
+//    }
+//
+//    public mutating func enqueue(_ element: T) {
+//        array.append(element)
+//    }
+//
+//    public mutating func add(_ item: T) {
+//        self.enqueue(item)
+//    }
+//
+//    public mutating func dequeue() -> T? {
+//        guard head < array.count, let element = array[head] else { return nil }
+//
+//        array[head] = nil
+//        head += 1
+//
+//        let percentage = Double(head)/Double(array.count)
+//        if array.count > 50 && percentage > 0.25 {
+//            array.removeFirst(head)
+//            head = 0
+//        }
+//
+//        return element
+//    }
+//
+//    public var front: T? {
+//        if isEmpty {
+//            return nil
+//        } else {
+//            return array[head]
+//        }
+//    }
+//
+//    public subscript(position: Int) -> T? {
+//        return array[position]
+//    }
+//
+//    public var description: String {
+//        return array.description
+//    }
+//}
--- BasicProtocolForContainers/ViewController.swift
+++ BasicProtocolForContainers/ViewController.swift
@@ -10,58 +10,92 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        testLinkedList()
+        testQueue()
+        
+    }
+    
+    func testLinkedList() {
         let dogBreeds = LinkedList<String>()
         dogBreeds.append(value: "Labrador")
         dogBreeds.append(value: "Bulldog")
         dogBreeds.append(value: "Beagle")
         dogBreeds.append(value: "Husky")
         dogBreeds.add("Reks")
-        print(dogBreeds)
-
+        dogBreeds.add("Muxtar")
+//        print(dogBreeds)
+//        print(dogBreeds.count)
+//        print(dogBreeds[10] ?? "None")
+//        print(dogBreeds[3] ?? "None")
+        
         let numbers = LinkedList<Int>()
         numbers.append(value: 5)
         numbers.append(value: 10)
         numbers.append(value: 15)
         numbers.add(21)
-        print(numbers)
-
-        print(dogBreeds.count)
-        print(numbers.count)
-
-        print(dogBreeds[10])
-        print(dogBreeds[3])
-
-        print(numbers[10])
-        print(numbers[3])
+        numbers.add(50)
+//        print(numbers)
+//        print(numbers.count)
+//        print(numbers[10] ?? "None")
+//        print(numbers[3] ?? "None")
         
+        let str1 = """
+        \(dogBreeds)
+        \(dogBreeds.count)
+        \(dogBreeds[10] ?? "None")
+        \(dogBreeds[3] ?? "None")
         
-        
-        
+        \(numbers)
+        \(numbers.count)
+        \(String(describing: numbers[10] ?? nil))
+        \(String(describing: numbers[3] ?? nil))
+        """
+        textView.text = str1
+    }
+    
+    
+    
+    func testQueue() {
         var queue = Queue<Int>()
         queue.enqueue(10)
         queue.enqueue(15)
         queue.enqueue(30)
         queue.add(77)
-        print(queue)
-        print(queue.count)
-        print(queue[3])
+        queue.add(100)
+//        print(queue)
+//        print(queue.count)
+//        print(queue[3] ?? "None")
         
         var queue2 = Queue<String>()
-        queue2.enqueue("mad")
-        queue2.enqueue("lad")
-        queue2.enqueue("sdfsdf")
-        queue2.add("tyjytj")
-        if let first = queue2.dequeue() {
-            print(first)
-        }
-        print(queue2)
+        queue2.enqueue("Flower")
+        queue2.enqueue("Car")
+        queue2.enqueue("Tree")
+        queue2.add("Toy")
+        queue2.add("Game")
+        guard let first = queue2.dequeue() else { return }
+           
+//        print(queue2)
+        
+        let str2 = """
+        
+        
+        \(queue)
+        \(queue.count)
+        \(String(describing: queue[3] ?? nil))
+        
+        \(first)
+        \(queue2)
+        """
+        textView.text.append(str2)
     }
-
-
+    
+    
+    
 }
 
